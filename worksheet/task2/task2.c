bfb704a7d698464793fcdeab2af1d52337dc6fdc
Splixson72 <145981456+Splixson72@users.noreply.github.com>
2026-03-26 23:50:57 +0000

    workshhet

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -14,12 +14,66 @@ int main(void){
 	char hex[9];
 	
 	printf("Enter a hexadecimal:");
-
+	int count = scanf("%8s", hex);
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
-	
-	// print the decimal result
+	int multiplier = 1;
+	int stringlength = strlen(hex);
+	for(int i = stringlength-1; i>=0;i--){
+		switch (hex[i]){
+			case '0':
+				decimal += multiplier*0;
+				break;
+			case '1':
+				decimal += multiplier*1;
+				break;
+			case '2':
+				decimal += multiplier*2;
+				break;
+			case '3':
+				decimal += multiplier*3;
+				break;
+			case '4':
+				decimal += multiplier*4;
+				break;
+			case '5':
+				decimal += multiplier*5;
+				break;
+			case '6':
+				decimal += multiplier*6;
+				break;
+			case '7':
+				decimal += multiplier*7;
+				break;
+			case '8':
+				decimal += multiplier*8;
+				break;
+			case '9':
+				decimal += multiplier*9;
+				break;
+			case 'A':
+				decimal += multiplier*10;
+				break;
+			case 'B':
+				decimal += multiplier*11;
+				break;
+			case 'C':
+				decimal += multiplier*12;
+				break;
+			case 'D':
+				decimal += multiplier*13;
+				break;
+			case 'E':
+				decimal += multiplier*14;
+				break;
+			case 'F':
+				decimal += multiplier*15;
+				break;
+			default:
+			printf("Error: Invalid Hexadecimal\n");
+		}
+		multiplier*=16;
+	}
 	printf("decimal:%ld\n", decimal);
-	
 	return 0;
 }
